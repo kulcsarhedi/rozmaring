3182058827b690a3540c9b600260d6b2a6ec89ec
Hedi Kulcsar <kulcsarhedi@gmail.com>
2026-06-12 20:04:39 +0200

    App isn't wrapped with I18nextProvider

--- src/index.js
+++ src/index.js
@@ -4,23 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
-import i18next from "i18next";
-import './i18n';
-
+import i18n from './i18n';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './index.css'; // Your global styles
 import { I18nextProvider } from 'react-i18next';
-import './i18n';
-
-
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
